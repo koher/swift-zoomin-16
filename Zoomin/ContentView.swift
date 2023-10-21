f77d0cef54c5f9092c43924f76b23473b3844247
Yuta Koshizawa <koher@koherent.org>
2023-10-21 18:15:22 +0900

    プロジェクトを作成

---
+++ Zoomin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zoomin
+//
+//  Created by Yuta Koshizawa on 2023/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zoomin/ZoominApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoominApp.swift
+//  Zoomin
+//
+//  Created by Yuta Koshizawa on 2023/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ZoominApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
